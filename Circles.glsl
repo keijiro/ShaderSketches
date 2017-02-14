87461e5de91ee12bd16bf8aebc39a7513511960d
Keijiro Takahashi <keijiro@gmail.com>
2017-02-14 19:40:20 +0900

    Refactoring to Circles.

--- Circles.glsl
+++ Circles.glsl
@@ -7,19 +7,22 @@ float circle(vec2 coord, vec2 offs)
     float d = distance(p, vec2(cw / 2.0));
 
     vec2 p2 = floor(coord / cw) - offs;
-
-    float t = iGlobalTime * 2.0 + p2.x * 0.44 + p2.y * 0.31;
+    vec2 gr = vec2(0.443, 0.312);
+    float t = iGlobalTime * 2.0 + dot(p2, gr);
 
     float l = cw * (sin(t) + 1.2) * 0.4;
     float lw = 1.5;
-
-    return saturate(1.0 - abs(l - d) / lw);
+    return max(0.0, 1.0 - abs(l - d) / lw);
 }
 
 void main()
 {
     float c = 0.0;
     for (int i = 0; i < 9; i++)
-        c += circle(gl_FragCoord.xy, vec2(mod(float(i), 3.0) - 1.0, float(i / 3) - 1.0));
-    gl_FragColor = vec4(vec3(saturate(c)), 1);
+    {
+        float dx = mod(float(i), 3.0) - 1.0;
+        float dy = float(i / 3) - 1.0;
+        c += circle(gl_FragCoord.xy, vec2(dx, dy));
+    }
+    gl_FragColor = vec4(vec3(min(1.0, c)), 1);
 }

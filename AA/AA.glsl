5444c2815dc98d75c5cf50be1c36c03da43f600f
Keijiro Takahashi <keijiro@gmail.com>
2017-02-24 14:28:29 +0900

    FXAA: finishing up.

--- AA/AA.glsl
+++ AA/AA.glsl
@@ -7,45 +7,59 @@ vec2 QuantizeUV(vec2 uv)
 
 float Luma(vec3 rgb)
 {
-    return dot(rgb, vec3(1.0 / 3.0));
+    return dot(rgb, vec3(0.2126, 0.7152, 0.0722));
 }
 
-vec3 AASample(vec2 uv)
+vec3 SampleTexture(vec2 uv)
 {
-    vec2 duv = vec2(1.0) / textureSize.xy;
-    const float bias = -1000.0;
+    return texture2D(iChannel0, uv, -1000.0).rgb;
+}
 
-    vec3 c0 = texture2D(iChannel0, uv).rgb;
+vec3 AASample(vec2 uv, bool force2tap, bool mask)
+{
+    vec2 duv = 1.0 / textureSize.xy;
+
+    vec3 c0 = SampleTexture(uv);
+
     float l0 = Luma(c0);
-    float l1 = Luma(texture2D(iChannel0, uv + duv * vec2(-0.5, -0.5), bias).rgb) + 1.0 / 384.0;
-    float l2 = Luma(texture2D(iChannel0, uv + duv * vec2(+0.5, -0.5), bias).rgb);
-    float l3 = Luma(texture2D(iChannel0, uv + duv * vec2(-0.5, +0.5), bias).rgb);
-    float l4 = Luma(texture2D(iChannel0, uv + duv * vec2(+0.5, +0.5), bias).rgb);
+    float l1 = Luma(SampleTexture(uv + duv * vec2(-0.5, -0.5))) + 1.0 / 384.0;
+    float l2 = Luma(SampleTexture(uv + duv * vec2(+0.5, -0.5)));
+    float l3 = Luma(SampleTexture(uv + duv * vec2(-0.5, +0.5)));
+    float l4 = Luma(SampleTexture(uv + duv * vec2(+0.5, +0.5)));
 
-    float max_luma = max(max(max(l1, l2), l3), l4);
-    float min_luma = min(min(min(l1, l2), l3), l4);
-    float contrast = max(l0, max_luma) - min(l0, min_luma);
-    if (contrast < max(0.05, 0.125 * max_luma)) return c0;
+    float l_max = max(max(max(l1, l2), l3), l4);
+    float l_min = min(min(min(l1, l2), l3), l4);
+    float contrast = max(l0, l_max) - min(l0, l_min);
+    if (contrast < max(0.05, 0.125 * l_max)) return c0;
+
+    if (mask) return vec3(1.0, 0.0, 0.0);
 
     vec2 dir1 = normalize(vec2((l3 + l4) - (l1 + l2), (l2 + l4) - (l1 + l3)));
-    vec2 dir2 = clamp(dir1 / min(abs(dir1.x), abs(dir1.y)) / 8.0, -2.0, 2.0);
+    vec2 dir2 = clamp(dir1 / (min(abs(dir1.x), abs(dir1.y)) * 8.0), -2.0, 2.0);
 
-    vec3 c11 = texture2D(iChannel0, uv - duv * dir1 * 0.5, bias).rgb;
-    vec3 c12 = texture2D(iChannel0, uv + duv * dir1 * 0.5, bias).rgb;
-    vec3 c21 = texture2D(iChannel0, uv - duv * dir2 * 2.0, bias).rgb;
-    vec3 c22 = texture2D(iChannel0, uv + duv * dir2 * 2.0, bias).rgb;
+    vec3 c1a = SampleTexture(uv - duv * dir1 * 0.5);
+    vec3 c1b = SampleTexture(uv + duv * dir1 * 0.5);
+    vec3 c2a = SampleTexture(uv - duv * dir2 * 2.0);
+    vec3 c2b = SampleTexture(uv + duv * dir2 * 2.0);
 
-    vec3 c1 = (c11 + c12) * 0.5;
-    vec3 c2 = (c11 + c12 + c21 + c22) * 0.25;
-    float lc2 = Luma(c2);
-    return (lc2 > min_luma && lc2 < max_luma) ? c2 : c1;
+    vec3 c_2tap = (c1a + c1b) * 0.5;
+    vec3 c_4tap = (c1a + c1b + c2a + c2b) * 0.25;
+    float l_4tap = Luma(c_4tap);
+    return (l_4tap < l_min || l_max < l_4tap || force2tap) ? c_2tap : c_4tap;
 }
 
 void main()
 {
-    vec2 uv = QuantizeUV(gl_FragCoord.xy / iResolution.xy);
-    vec3 c1 = texture2D(iChannel0, uv).rgb;
-    vec3 c2 = AASample(uv);
-    float p = saturate(0.5 - cos(iGlobalTime) * 2.0);
-    gl_FragColor.rgb = mix(c1, c2, p);
+    vec2 uv = QuantizeUV(gl_FragCoord.xy / iResolution.xy * 2.0);
+
+    if (uv.y < 1.0)
+        if (uv.x < 1.0)
+            gl_FragColor.rgb = AASample(fract(uv), true, false);
+        else
+            gl_FragColor.rgb = AASample(fract(uv), false, false);
+    else
+        if (uv.x < 1.0)
+            gl_FragColor.rgb = SampleTexture(fract(uv));
+        else
+            gl_FragColor.rgb = AASample(fract(uv), false, true);
 }

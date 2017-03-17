3ee7d0c443559f479a29c745e157ce41726d9194
Keijiro Takahashi <keijiro@gmail.com>
2017-03-17 10:25:16 +0900

    Hex3

---
+++ Hex3.glsl
@@ -0,0 +1,23 @@
+#version 150
+
+uniform float time;
+uniform vec2 resolution;
+out vec4 fragColor;
+
+void main(void)
+{
+    const float pi = 3.1415926535;
+    vec2 p = gl_FragCoord.xy - resolution / 2;
+    float phi = atan(p.y, p.x + 1e-5);
+
+    float fin = mod(floor(phi * 3 / pi + 0.5), 6);
+    float phi_fin = fin * pi / 3;
+
+    vec2 dir = vec2(cos(phi_fin), sin(phi_fin));
+    float l = dot(dir, p) - time * resolution.y / 8;
+    float seg = floor(l * 70 / resolution.y);
+
+    float th = sin(time) * 0.4 + 0.5;
+    float t = sin(seg * 92.198763) * time;
+    fragColor = vec4(step(th, fract(phi / pi / 2 + t)));
+}
